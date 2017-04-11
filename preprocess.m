e792228be5cbb50b8c69134adb2eba4bec9d45d7
CooperL <cooper.loughlin@gmail.com>
2017-04-11 18:44:06 -0400

    progress on frequency identification

---
+++ preprocess.m
@@ -0,0 +1,12 @@
+function [y,s,Fs] = preprocess(filename,N)
+%PREPROCESS get audio file and highpass filter spectrogram
+
+[y,Fs] = audioread(filename,[1,N]);
+Nw = Fs*32e-3;
+noverlap = Nw-Fs*1e-3;
+Nfft = 512;
+s = spectrogram(y,hamming(Nw),noverlap,Nfft);
+figure;
+spectrogram(y,hamming(Nw),noverlap,Nfft,Fs,'yaxis')
+end
+
---
+++ simulation.m
@@ -0,0 +1,18 @@
+clear; clc; close all
+%% load audio files
+filestr1 = 'speech_samples/OSR_us_000_00'; filestr2 = '_8k.wav';
+filenum1 = 10;
+filenum2 = 11;
+file1 = [filestr1,num2str(filenum1),filestr2];
+file2 = [filestr1,num2str(filenum2),filestr2];
+N = 10000;
+[y1,s1,Fs] = preprocess(file1,N);
+[y2,s2,~] = preprocess(file2,N);
+y1 = y1(1:N); y2 = y2(1:N); 
+%% estimate pitch
+freqwidth = 875; % Hz
+timewidth = 20e-3; % s
+freqjump = 140; % Hz
+timejump = 5e-3; % s
+
+p1 = pitch_est(s1,Fs,freqwidth,timewidth,freqjump,timejump);
